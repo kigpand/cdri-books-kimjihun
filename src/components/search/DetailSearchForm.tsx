7ab7fa2156a7f64a7e925295e42d636c4264b6f2
김지훈 <kigpand@gmail.com>
2025-06-30 19:16:08 +0900

    feat: 상세검색 UI 완성

--- src/components/search/DetailSearchForm.tsx
+++ src/components/search/DetailSearchForm.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import type { DETAIL_SEARCH_TRAGET } from "../../api/book";
 import Button from "../common/Button";
+import SelectBox from "../common/Selectbox";
+
+const options: { label: string; value: string }[] = [
+  { label: "저자명", value: "person" },
+  { label: "출판사", value: "publisher" },
+];
 
 type Props = {
   handleChangeSearchTarget: (
@@ -10,21 +16,41 @@ type Props = {
 };
 
 export default function DetailSearchForm({ handleChangeSearchTarget }: Props) {
-  const [selectValue, setSelectValue] =
-    useState<DETAIL_SEARCH_TRAGET>("person");
+  const [selectValue, setSelectValue] = useState<{
+    label: string;
+    value: string;
+  }>(options[0]);
   const [keyword, setKeyword] = useState<string>("");
 
   function handleSearchButton() {
     if (keyword === "") return;
-    handleChangeSearchTarget(selectValue, keyword);
+    handleChangeSearchTarget(
+      selectValue.value as DETAIL_SEARCH_TRAGET,
+      keyword
+    );
+    setKeyword("");
   }
 
   return (
     <div
       role="dialog"
-      className="absolute bg-white top-[51px] w-[360px] h-40 shadow-modal rounded-[8px] z-[1px] flex items-center justify-center"
+      className="absolute bg-white top-[51px] w-[360px] px-6 py-9 h-40 shadow-modal rounded-[8px] z-[1px]"
     >
-      <div></div>
+      <div className="w-full flex gap-1 mb-4">
+        <SelectBox
+          className="w-[100px] h-9"
+          values={options}
+          currentItem={selectValue}
+          handleChangeSelect={setSelectValue}
+        />
+        <input
+          type="text"
+          className="grow h-9 border-b border-palette-primary outline-none text-caption text-[#8d94a0]"
+          value={keyword}
+          placeholder="검색어를 입력하세요"
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </div>
       <Button className="w-full py-[5px]" onClick={handleSearchButton}>
         검색하기
       </Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,7 @@ module.exports = {
       },
       boxShadow: {
         modal: "0px 4px 14px 6px rgba(151, 151, 151, 0.15)",
+        selectItembox: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
       },
     },
   },
